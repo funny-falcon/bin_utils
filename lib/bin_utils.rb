c6d48ce77a5a58f422a88240f7b7918c2364ea73
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2014-10-28 15:36:32 +0300

    BinUtiles includeable

--- lib/bin_utils.rb
+++ lib/bin_utils.rb
@@ -8,10 +8,12 @@ module BinUtils
       require 'bin_utils/bin_utils'
     end
     extend ::BinUtils::Native
+    include ::BinUtils::Native
   rescue LoadError
     raise
     require 'bin_utils/pure_ruby'
-    extend PureRuby
+    extend ::BinUtils::PureRuby
+    include ::BinUtils::PureRuby
     if RUBY_ENGINE == 'ruby' || RUBY_ENGINE == 'jruby'
       $stderr.puts "Attention: pure ruby version of BinUtils is used"
     end
