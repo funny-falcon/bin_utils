eb4a8d6ade4617a1bf97a21303250a85d98f60c1
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2014-10-28 15:37:41 +0300

    fallback to PureRuby if compiller missing

--- ext/bin_utils/extconf.rb
+++ ext/bin_utils/extconf.rb
@@ -1,9 +1,12 @@
-if RUBY_ENGINE == 'ruby' || RUBY_ENGINE == 'rbx'
+begin
+  unless RUBY_ENGINE == 'ruby' || RUBY_ENGINE == 'rbx'
+    raise "Uh"
+  end
   require 'mkmf'
   # rubinius has no rb_str_drop_bytes
   have_func('rb_str_drop_bytes')
   create_makefile("bin_utils")
-else
+rescue
   File.open(File.dirname(__FILE__) + "/Makefile", 'w') do |f|
     f.write("install:\n\t#nothing to build")
   end
